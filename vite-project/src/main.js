d98c2e4ef2f7ab3596876b08b36f75b6379bfca3
S29Ducky <163264380+S29Ducky@users.noreply.github.com>
2025-06-23 17:01:35 +0200

    Update HTML structure for daily bee and honey data display; enhance data fetching logic in main.js

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -4,8 +4,24 @@ const temp = document.querySelector("#temp");
 const flow = document.querySelector("#flow");
 const flowMoyenne = document.querySelector("#flowMoyenne");
 
-function tab1() {
+async function appelTab1() {
+	const appel = await fetch("http://10.69.0.140:3000/history/honey", {
+		method: "GET",
+	});
+	const data = await appel.json();
+	let newArr = [];
+
+	for (let i = 0; i < 7; i++) {
+		newArr.push(data[i].weight);
+	}
+	console.log(newArr);
+	return newArr;
+}
+
+async function tab1() {
 	const ctx = document.getElementById("myChart");
+
+	const dataHistory = await appelTab2();
 
 	const labels = [
 		"Lundi",
@@ -21,7 +37,7 @@ function tab1() {
 		datasets: [
 			{
 				label: "Miel produit (kg)",
-				data: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
+				data: dataHistory,
 				fill: false,
 				borderColor: "rgb(240, 183, 59)",
 				tension: 0.1,
@@ -43,16 +59,9 @@ function tab1() {
 			},
 		},
 	};
-
-	async function AppelTb1() {
-		const appel = await fetch("http://10.69.0.140:3000/history/honey", {
-			method: "GET",
-		});
-		const data = await appel.json();
-	}
-
 	new Chart(ctx, config);
 }
+
 tab1();
 
 async function temperature() {
@@ -80,7 +89,6 @@ async function flowBee() {
 	});
 	const data = await FlowAppel.json();
 	flow.textContent = data.count;
-	flowMoyenne.textContent = `Prédiction: ${data.count}`;
 }
 flowBee();
 
@@ -101,7 +109,7 @@ async function flowMoyenneBee() {
 	}
 	let result = somme / newArr.length;
 	console.log(result);
-	flowMoyenne.textContent = `Prédiction: ${result}`;
+	flowMoyenne.textContent = `Prédiction: ${result.toFixed(2)}`;
 }
 
 flowMoyenneBee();
@@ -114,14 +122,18 @@ async function appelTab2() {
 	let newArr = [];
 
 	for (let i = 0; i < data.length; i++) {
-		newArr.push(data[i].weigth);
+		newArr.push(data[i].weight);
 	}
+	console.log(newArr);
+	return newArr;
 }
 
-function tab2() {
-	appelTab2();
-
+async function tab2() {
 	const ctx = document.querySelector("#TabHistorique");
+
+	const dataHistory = await appelTab2();
+
+	console.log(dataHistory);
 
 	const label = [
 		"Lundi",
@@ -159,10 +171,7 @@ function tab2() {
 		datasets: [
 			{
 				label: "Miel produit (kg)",
-				data: [
-					0, 2, 4, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32,
-					34, 36, 38, 40, 42, 44, 46, 48, 50,
-				],
+				data: dataHistory,
 				backgroundColor: [
 					"rgb(240, 183, 59)",
 					"rgb(240, 183, 59)",
@@ -214,3 +223,129 @@ function tab2() {
 }
 
 tab2();
+
+let abeilleJour1 = document.querySelector("#nbr-abeille1");
+let abeilleJour2 = document.querySelector("#nbr-abeille2");
+let abeilleJour3 = document.querySelector("#nbr-abeille3");
+let abeilleJour4 = document.querySelector("#nbr-abeille4");
+let abeilleJour5 = document.querySelector("#nbr-abeille5");
+let abeilleJour6 = document.querySelector("#nbr-abeille6");
+let abeilleJour7 = document.querySelector("#nbr-abeille7");
+
+async function flowBeePerDay() {
+	const FlowAppel = await fetch("http://10.69.0.140:3000/history/flow", {
+		method: "GET",
+	});
+	const data = await FlowAppel.json();
+
+	let newArr = [];
+
+	for (let i = 0; i < data.length; i++) {
+		newArr.push(data[i].count);
+	}
+
+	abeilleJour1.textContent = newArr[0];
+	abeilleJour2.textContent = newArr[1];
+	abeilleJour3.textContent = newArr[2];
+	abeilleJour4.textContent = newArr[3];
+	abeilleJour5.textContent = newArr[4];
+	abeilleJour6.textContent = newArr[5];
+	abeilleJour7.textContent = newArr[6];
+}
+flowBeePerDay();
+
+let mielJour1 = document.querySelector("#nbr-rendement1");
+let mielJour2 = document.querySelector("#nbr-rendement2");
+let mielJour3 = document.querySelector("#nbr-rendement3");
+let mielJour4 = document.querySelector("#nbr-rendement4");
+let mielJour5 = document.querySelector("#nbr-rendement5");
+let mielJour6 = document.querySelector("#nbr-rendement6");
+let mielJour7 = document.querySelector("#nbr-rendement7");
+
+async function honeyPerDay() {
+	const FlowAppel = await fetch("http://10.69.0.140:3000/history/honey", {
+		method: "GET",
+	});
+	const data = await FlowAppel.json();
+
+	let newArr = [];
+
+	for (let i = 0; i < data.length; i++) {
+		newArr.push(data[i].weight);
+	}
+
+	mielJour1.textContent = newArr[0] + " Kg";
+	mielJour2.textContent = newArr[1] + " Kg";
+	mielJour3.textContent = newArr[2] + " Kg";
+	mielJour4.textContent = newArr[3] + " Kg";
+	mielJour5.textContent = newArr[4] + " Kg";
+	mielJour6.textContent = newArr[5] + " Kg";
+	mielJour7.textContent = newArr[6] + " Kg";
+}
+honeyPerDay();
+
+let mielSemaine = document.querySelector("#mielMoyenne");
+
+async function honeyPerWeek() {
+	const FlowAppel = await fetch("http://10.69.0.140:3000/history/honey", {
+		method: "GET",
+	});
+	const data = await FlowAppel.json();
+	let somme = 0;
+	let newArr = [];
+
+	for (let i = 0; i < 7; i++) {
+		newArr.push(data[i].weight);
+	}
+
+	for (let i = 0; i < newArr.length; i++) {
+		somme += newArr[i];
+	}
+	let result = somme / newArr.length;
+	mielSemaine.textContent = result.toFixed(2) + " Kg";
+}
+honeyPerWeek();
+
+let abeilleSemaine = document.querySelector("#AbeillesMoyenne");
+
+async function beesPerWeek() {
+	const FlowAppel = await fetch("http://10.69.0.140:3000/history/flow", {
+		method: "GET",
+	});
+	const data = await FlowAppel.json();
+	let somme = 0;
+	let newArr = [];
+
+	for (let i = 0; i < 7; i++) {
+		newArr.push(data[i].count);
+	}
+
+	for (let i = 0; i < newArr.length; i++) {
+		somme += newArr[i];
+	}
+	let result = somme / newArr.length;
+	abeilleSemaine.textContent = result;
+}
+beesPerWeek();
+
+let tempSemaine = document.querySelector("#TempMoyenne");
+
+async function tempPerWeek() {
+	const FlowAppel = await fetch("http://10.69.0.140:3000/history/temperature", {
+		method: "GET",
+	});
+	const data = await FlowAppel.json();
+	let somme = 0;
+	let newArr = [];
+
+	for (let i = 0; i < 7; i++) {
+		newArr.push(data[i].degree);
+	}
+
+	for (let i = 0; i < newArr.length; i++) {
+		somme += newArr[i];
+	}
+	let result = somme / newArr.length;
+	tempSemaine.textContent = result.toFixed(2) + " °C";
+}
+tempPerWeek();
